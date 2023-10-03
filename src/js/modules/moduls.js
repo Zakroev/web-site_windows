a43d19f7abd7256c62a8a7b76b6f2ae568a74a03
Zakroev Andrey <Dr.zakroev@yandex.ru>
2023-10-03 19:05:09 +0300

    Added a close window button and fix code from comments

--- src/js/modules/moduls.js
+++ src/js/modules/moduls.js
@@ -1,34 +1,42 @@
 const modals = () => {
   function bindModal(triggerSelector, modalSelector, closeSelector) {
-    const trigger = document.querySelectorAll(triggerSelector),
-      modal = document.querySelector(modalSelector),
-      close = document.querySelector(closeSelector)
-    trigger.forEach((item) => {
-      item.addEventListener('click', (e) => {
+    const triggers = document.querySelectorAll(triggerSelector)
+    const modal = document.querySelector(modalSelector)
+    const close = document.querySelector(closeSelector)
+
+    const closeModal = () => {
+      modal.style.display = 'none'
+      document.body.classList.remove('modal-open')
+    }
+
+    triggers.forEach((trigger) => {
+      trigger.addEventListener('click', (e) => {
         if (e.target) {
           e.preventDefault()
         }
         modal.style.display = 'block'
-        // document.body.style.overflow = 'hidden'
         document.body.classList.add('modal-open')
       })
     })
+
     close.addEventListener('click', () => {
-      modal.style.display = 'none'
-      // document.body.style.overflow = ''
-      document.body.classList.remove('modal-open')
+      closeModal()
     })
 
     modal.addEventListener('click', (e) => {
       if (e.target === modal) {
-        modal.style.display = 'none'
-        // document.body.style.overflow = ''
-        document.body.classList.remove('modal-open')
+        closeModal()
+      }
+    })
+
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        closeModal()
       }
     })
   }
 
-  function showModalBytime(selector, time) {
+  const showModalBytime = (selector, time) => {
     setTimeout(() => {
       document.querySelector(selector).style.display = 'block'
       document.body.style.overflow = ''
